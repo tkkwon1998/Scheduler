1693e1d326ff65f3561141a7c8ee6e1746183276
tkkwon1998 <tkkwon1998@gmail.com>
2020-09-28 23:48:14 -0500

    Detail screen

--- App.js
+++ App.js
@@ -1,116 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ScheduleScreen from './screens/ScheduleScreen';
 
-import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import CourseList from './components/CourseList';  
-
-
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": [
-    {
-      "id": "F101",
-      "title": "Computer Science: Concepts, Philosophy, and Connections",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F110",
-      "title": "Intro Programming for non-majors",
-      "meets": "MWF 10:00-10:50"
-    },
-    {
-      "id": "F111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 13:00-13:50"
-    },
-    {
-      "id": "F211",
-      "title": "Fundamentals of Computer Programming II",
-      "meets": "TuTh 12:30-13:50"
-    }
-  ]
-};
-
-const getCourseNumber = course => (
-  course.id.slice(1)
-);
-
-// const Course = ({ course }) => (
-//   <TouchableOpacity style={styles.courseButton}>
-//     <Text style={styles.courseText}>
-//       {`CS ${getCourseNumber(course)}\n${course.meets}`}
-//     </Text>
-//   </TouchableOpacity>
-// );
-
-// const CourseList = ({ courses }) => (
-//   <ScrollView>
-//     <View style={styles.courseList}>
-//       { courses.map(course => <Course key={course.id} course={ course } />) }
-//     </View>
-//   </ScrollView>
-// );
-
-const Banner = ({title}) => (
-  <Text style={styles.banner}>{title || '[loading...]'}</Text>
-);
+const Stack = createStackNavigator();
 
 const App = () => {
-  const [schedule, setSchedule] = useState({ title: '', courses: [] });
-  
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
-
-  useEffect(() => {
-    const fetchSchedule =  async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(json);
-    }
-    fetchSchedule();
-  }, []);
-  
   return (
-    <SafeAreaView style={styles.container}>
-      <Banner title={schedule.title} />
-      <CourseList courses={schedule.courses} />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="ScheduleScreen"
+          component={ScheduleScreen}
+          options={{ title: 'Schedule'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-  
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 20,
-  },
-  banner: {
-    color: '#888',
-    fontSize: 32,
-  },
-  courseList: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  courseButton: {
-    borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 10,
-    height: 60,
-    padding: 10,
-    minWidth: 90,
-    maxWidth: 90,
-    backgroundColor: '#66b0ff',
-  },
-  courseText:{
-    color: '#fff',
-    fontSize: 12,
-    textAlign: 'center',
-  },
-});
+
 
 export default App;
---
+++ screens/ScheduleScreen.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react';
+
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import CourseList from '../components/CourseList';  
+
+
+const schedule = {
+  "title": "CS Courses for 2018-2019",
+  "courses": [
+    {
+      "id": "F101",
+      "title": "Computer Science: Concepts, Philosophy, and Connections",
+      "meets": "MWF 11:00-11:50"
+    },
+    {
+      "id": "F110",
+      "title": "Intro Programming for non-majors",
+      "meets": "MWF 10:00-10:50"
+    },
+    {
+      "id": "F111",
+      "title": "Fundamentals of Computer Programming I",
+      "meets": "MWF 13:00-13:50"
+    },
+    {
+      "id": "F211",
+      "title": "Fundamentals of Computer Programming II",
+      "meets": "TuTh 12:30-13:50"
+    }
+  ]
+};
+
+const getCourseNumber = course => (
+  course.id.slice(1)
+);
+
+// const Course = ({ course }) => (
+//   <TouchableOpacity style={styles.courseButton}>
+//     <Text style={styles.courseText}>
+//       {`CS ${getCourseNumber(course)}\n${course.meets}`}
+//     </Text>
+//   </TouchableOpacity>
+// );
+
+// const CourseList = ({ courses }) => (
+//   <ScrollView>
+//     <View style={styles.courseList}>
+//       { courses.map(course => <Course key={course.id} course={ course } />) }
+//     </View>
+//   </ScrollView>
+// );
+
+const Banner = ({title}) => (
+  <Text style={styles.banner}>{title || '[loading...]'}</Text>
+);
+
+const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+  
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule =  async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, []);
+  
+  return (
+    <SafeAreaView style={styles.container}>
+      <Banner title={schedule.title} />
+      <CourseList courses={schedule.courses} />
+    </SafeAreaView>
+  );
+};
+  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 20,
+  },
+  banner: {
+    color: '#888',
+    fontSize: 32,
+  },
+  courseList: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  courseButton: {
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    height: 60,
+    padding: 10,
+    minWidth: 90,
+    maxWidth: 90,
+    backgroundColor: '#66b0ff',
+  },
+  courseText:{
+    color: '#fff',
+    fontSize: 12,
+    textAlign: 'center',
+  },
+});
+
+export default App;

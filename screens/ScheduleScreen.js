512fdf8d00f211e72509b807360d6b068a7ab255
tkkwon1998 <tkkwon1998@gmail.com>
2020-09-30 12:33:08 -0500

    Added firebase

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -4,6 +4,7 @@ import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } fr
 import CourseList from '../components/CourseList';  
 import UserContext from '../UserContext';
 import CourseEditScreen from './CourseEditScreen';
+import { firebase } from '../firebase';
 
 
 
@@ -57,6 +58,11 @@ const Banner = ({title}) => (
   <Text style={styles.banner}>{title || '[loading...]'}</Text>
 );
 
+const fixCourses = json => ({
+    ...json,
+    courses: Object.values(json.courses)
+  });
+
 const ScheduleScreen = ({navigation}) => {
     const user = useContext(UserContext);
     const canEdit = user && user.role === 'admin';
@@ -67,13 +73,12 @@ const ScheduleScreen = ({navigation}) => {
   const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
 
   useEffect(() => {
-    const fetchSchedule =  async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(json);
+    const db = firebase.database().ref();
+    const handleData = snap => {
+      if (snap.val()) setSchedule(fixCourses(snap.val()));
     }
-    fetchSchedule();
+    db.on('value', handleData, error => alert(error));
+    return () => { db.off('value', handleData); };
   }, []);
   
   return (

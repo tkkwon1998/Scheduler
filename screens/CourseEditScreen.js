95e8960013a72361669d378db8f7026d63ad1ecf
tkkwon1998 <tkkwon1998@gmail.com>
2020-10-03 10:25:54 -0500

    Firebase update

--- screens/CourseEditScreen.js
+++ screens/CourseEditScreen.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
-import Form from '../Form';
-import * as Yup from 'yup';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import Form from "../Form";
+import * as Yup from "yup";
+import { firebase } from "../firebase";
+import React, { useState, useEffect } from "react";
 
-const Field = ({label, value}) => {
+const Field = ({ label, value }) => {
   return (
     <View style={styles.fieldContainer}>
       <Text style={styles.label}>{label}</Text>
@@ -15,30 +16,49 @@ const Field = ({label, value}) => {
 const validationSchema = Yup.object().shape({
   id: Yup.string()
     .required()
-    .matches(/(F|W|S)\d{3,}/, 'Must be a term and 3-digit number')
-    .label('ID'),
+    .matches(/(F|W|S)\d{3,}/, "Must be a term and 3-digit number")
+    .label("ID"),
   meets: Yup.string()
     .required()
-    .matches(/(M|Tu|W|Th|F)+ +\d\d?:\d\d-\d\d?:\d\d/, 'Must be weekdays followed by start and end time')
-    .label('Meeting times'),
+    .matches(
+      /(M|Tu|W|Th|F)+ +\d\d?:\d\d-\d\d?:\d\d/,
+      "Must be weekdays followed by start and end time"
+    )
+    .label("Meeting times"),
   title: Yup.string()
     .required()
-    .label('Title'),
+    .label("Title")
 });
 
-const CourseEditScreen = ({navigation, route}) => {
+const CourseEditScreen = ({ navigation, route }) => {
   const course = route.params.course;
+  const [submitError, setSubmitError] = useState("");
+
+  async function handleSubmit(values) {
+    const { id, meets, title } = values;
+    const course = { id, meets, title };
+    firebase
+      .database()
+      .ref("courses")
+      .child(id)
+      .set(course)
+      .catch(error => {
+        setSubmitError(error.message);
+      });
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-      <Form
-        validationSchema={validationSchema}
+        <Form
+          validationSchema={validationSchema}
           initialValues={{
             id: course.id,
             meets: course.meets,
-            title: course.title,
+            title: course.title
           }}
+          validationSchema={validationSchema}
+          onSubmit={values => handleSubmit(values)}
         >
           <Form.Field
             name="id"
@@ -58,6 +78,8 @@ const CourseEditScreen = ({navigation, route}) => {
             leftIcon="format-title"
             placeholder="Introduction to programming"
           />
+          <Form.Button title={"Update"} />
+          {<Form.ErrorMessage error={submitError} visible={true} />}
         </Form>
       </ScrollView>
     </SafeAreaView>
@@ -67,29 +89,29 @@ const CourseEditScreen = ({navigation, route}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#ccccb3'
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ccccb3"
   },
   field: {
     height: 40,
     width: 300,
     padding: 5,
-    backgroundColor: 'white',
+    backgroundColor: "white"
   },
   fieldContainer: {
     marginBottom: 20,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 2,
+      height: 2
     },
     shadowOpacity: 0.23,
     shadowRadius: 2.62,
-    elevation: 4,
+    elevation: 4
   },
   label: {
-    fontWeight: 'bold',
+    fontWeight: "bold"
   }
 });
 
